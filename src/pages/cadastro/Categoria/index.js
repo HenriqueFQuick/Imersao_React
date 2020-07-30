4d05c5df9045cfb1f21c3210a732c249add861ae
Henrique Fernandes <henrique.fquick@gmail.com>
2020-07-30 20:02:57 -0300

    Alteracoes para url prod/hml

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
     }
 
     useEffect(() => {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://henrique-aluraflix.herokuapp.com//categorias';
       fetch(URL).then(async (res) => {
         const resposta = await res.json();
         setCategorias([
